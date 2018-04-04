a869ee5060df898dad481c470636e0758ddc18be
taichi <taichi730@gmail.com>
2018-04-04 15:22:08 +0900

    added a switch to enable/disable default sequence

--- src/seq/tue_sequencer.svh
+++ src/seq/tue_sequencer.svh
@@ -32,9 +32,25 @@ class tue_sequencer #(
     this_type, PROXY_CONFIGURATION, PROXY_STATUS
   ) t_component_proxy;
 
+  protected bit enable_default_sequence = 1;
+
   function new(string name = "tue_sequencer", uvm_component parent = null);
     super.new(name, parent);
     void'(t_component_proxy::create_component_proxy(this));
+  endfunction
+
+  function void set_enable_default_sequence(bit value);
+    enable_default_sequence = value;
+  endfunction
+
+  function bit get_enable_default_sequence();
+    return enable_default_sequence;
+  endfunction
+
+  function void start_phase_sequence(uvm_phase phase);
+    if (enable_default_sequence) begin
+      super.start_phase_sequence(phase);
+    end
   endfunction
 
   `uvm_component_param_utils(tue_sequencer #(
